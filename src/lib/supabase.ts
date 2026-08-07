5e3b8901f09e7c1363e0dc37f207e7330832dc25
Joel Martinez <joel_martinez@outlook.com>
2026-08-07 08:48:34 -0300

    ajuste para funcionar no lovable

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./database.types";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+//const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+//const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+const supabaseUrl = "https://ytgmkmerzfjkdxldbolo.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0Z21rbWVyemZqa2R4bGRib2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMDI4MTYsImV4cCI6MjEwMTU3ODgxNn0.BmyLysJJO63bHoZXemSeCM2eqXhP1j-NrmUuLns4Ih4";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
